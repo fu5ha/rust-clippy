5983897ca594f0270acf2cd8194f602380c0b28c
Matthias Beyer <mail@beyermatthias.de>
2025-05-15 09:55:49 +0200

    Add flake setup for dev env
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Helper flake for rust dev envs";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+
+        rustTarget = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [
+            rustTarget
+
+            pkgs.openssl
+            pkgs.pkg-config
+          ];
+        };
+      }
+    );
+}
